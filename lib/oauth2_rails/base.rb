a9b1288dd04b1c59699e22677a8656d2d77b1c1e
Bob McCown <bob.mccown@goodmeasures.com>
2015-12-23 10:45:20 -0500

    Change defaults

--- lib/oauth2_rails/base.rb
+++ lib/oauth2_rails/base.rb
@@ -11,7 +11,7 @@ module Oauth2Rails
       @authorize_path = options[:authorize_path] || '/oauth2/authorize'
       @api_site       = options[:api_site]       || 'https://api.fitbit.com'
       @token_path     = options[:token_path]     || '/oauth2/token'
-      @scope          = options[:scope]          || 'heartrate'
+      @scope          = options[:scope]          || 'activity heartrate location nutrition profile settings sleep social weight'
     end
 
     def connection(url)
