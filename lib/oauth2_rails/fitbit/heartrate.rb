3597088f758512ecb7fa1968ac38caa430e71950
Ben Gerber <bgerber@uic.edu>
2016-02-26 19:50:23 -0600

    get heart rate using date range

--- lib/oauth2_rails/fitbit/heartrate.rb
+++ lib/oauth2_rails/fitbit/heartrate.rb
@@ -3,6 +3,10 @@ module HeartRate
   def daily_heart(start_date)
     api_call("/1/user/-/activities/heart/date/#{start_date}/1d.json")
   end
+  
+  def daily_heart_range(start_date, end_date)
+    api_call("/1/user/-/activities/heart/date/#{start_date}/#{end_date}.json")
+  end
 
   def minute_heart(days, seconds, start_date, start_time, end_time)
     api_call("/1/user/-/activities/heart/date/#{start_date}/#{days}d/#{seconds}sec/time/#{start_time}/#{end_time}.json")
